79b74bd38141248bf4f4e0493e09c818832486bc
Guru-Devops-creator <gurut591@gmail.com>
2025-01-17 17:34:37 +0530

    terraform

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,6 @@
 module "ec2" {
-    source = "../terraform-aws-ec2"
+    #source = "../terraform-aws-ec2"
+    source = "git::git@github.com:Guru-Devops-creator/terraform-aws-ec2-instance.git?ref=main"
     # these are variables expected module, not arguments of resource definition
     # You can also create variables or else you can directly use values here
     ami_id = var.ami_daws81s
